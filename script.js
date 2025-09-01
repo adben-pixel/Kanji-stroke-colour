4546c5fbbb236ff15d5484cffc112357688aae41
adben-pixel <adambstearns@gmail.com>
2025-09-01 22:59:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const video = document.getElementById("video");
 
 // make every button play its video
+console.log("Found buttons:", document.querySelectorAll(".kanji-btn").length);
+
 document.querySelectorAll(".kanji-btn").forEach(btn => {
   btn.addEventListener("click", () => {
     const file = btn.dataset.file; // get the filename from the button
